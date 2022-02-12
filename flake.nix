a1bf468c16f3dfde308b65a6c0f2c62ef1c5f900
Andrey Petrov <andrey.petrov@shazow.net>
2022-02-12 15:53:08 -0500

    flake.nix: Switch to foundry.nix overlay

--- flake.nix
+++ flake.nix
@@ -2,23 +2,28 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
-    foundry.url = "git+https://github.com/shazow/nixfiles?dir=flakes/foundry";
+    foundry.url = "github:shazow/foundry.nix";
   };
 
   outputs = { self, nixpkgs, utils, foundry }:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ foundry.overlay ];
+        };
       in {
 
         devShell = with pkgs; mkShell {
           buildInputs = [
             solc
-            foundry.defaultPackage.${system}
+            foundry-bin # From the foundry overlay
           ];
 
           shellHook = ''
             export PS1="\e[1;33m\][dev]\e[1;34m\] \w $ \e[0m\]"
+
+            [[ -f .env ]] && source .env
           '';
         };
 
